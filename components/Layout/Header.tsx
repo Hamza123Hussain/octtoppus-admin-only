11849dd782952856dd630f941cc9edc3e214505c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-09 04:07:17 +0500

    new signup details

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -2,7 +2,13 @@ import Image from 'next/image'
 import React from 'react'
 
 const Header = () => {
-  return <Image width={250} height={250} src="/Logo.png" alt="Logo" />
+  return (
+    <div className=" flex flex-col justify-center items-center gap-2">
+      {' '}
+      <Image width={250} height={250} src="/Logo.png" alt="Logo" />
+      <h1 className=" text-3xl font-extrabold">Admin Panel</h1>
+    </div>
+  )
 }
 
 export default Header
