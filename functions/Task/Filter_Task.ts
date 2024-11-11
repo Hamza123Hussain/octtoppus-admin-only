f65e1b619daeec3f5e05f9594258ce095dcd5eac
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-11-11 13:51:49 +0500

    Filtering tasks and showing todo first

--- functions/Task/Filter_Task.ts
+++ functions/Task/Filter_Task.ts
@@ -1,14 +1,12 @@
-import { TaskFetch } from '@/utils/TaskformInterface'
-
 export const filteredTasks = (
   allTasks: TaskFetch[],
   statusFilter: string,
   timeFilter: string,
   priorityFilter: string,
-  monthFilter: number, // New parameter for month filtering as a number
+  monthFilter: number,
   selectedUser?: string
-) =>
-  allTasks.filter((task) => {
+) => {
+  const filtered = allTasks.filter((task) => {
     const matchesTimeFilter =
       timeFilter === 'All' || task.TaskType === timeFilter
 
@@ -20,6 +18,7 @@ export const filteredTasks = (
 
     const selectedUserFilter =
       selectedUser === 'All' || selectedUser === task.assignedTo
+
     // Month filtering logic
     const taskDate = new Date(task.createdAt)
     const taskMonth = taskDate.getMonth() // Get month as a number (0-11)
@@ -33,3 +32,11 @@ export const filteredTasks = (
       selectedUserFilter
     )
   })
+
+  // Sort the tasks so that "TODO" tasks come first
+  return filtered.sort((a, b) => {
+    if (a.progress === 'TODO' && b.progress !== 'TODO') return -1 // 'TODO' tasks should come first
+    if (b.progress === 'TODO' && a.progress !== 'TODO') return 1
+    return 0 // No change in order if both are the same or neither is "TODO"
+  })
+}
