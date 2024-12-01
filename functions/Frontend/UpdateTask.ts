fdd3c469623087063d48dca96140eba0cd971312
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-01 17:56:42 +0500

    needed

--- functions/Frontend/UpdateTask.ts
+++ functions/Frontend/UpdateTask.ts
@@ -42,7 +42,7 @@ export const handleUpdateTask = async (
     )
     if (UpdateTask) {
       toast.success('Task updated successfully!')
-      Router.push('/usertasks') // Use Router for navigation
+      Router.push('/') // Use Router for navigation
     }
   } catch (error) {
     toast.error(`Error updating task: ${error}`)
